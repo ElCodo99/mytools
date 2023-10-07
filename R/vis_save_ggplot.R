077560688cfb7bea87f6a484ec8268d5ee125900
ElCodo99 <98619567+ElCodo99@users.noreply.github.com>
2023-10-07 21:18:02 +0200

    added further arguments to save_ggplot

--- R/vis_save_ggplot.R
+++ R/vis_save_ggplot.R
@@ -1,26 +1,35 @@
-#' Save the Last ggplot Object Quietly
+#' Save ggplot2 Plots with Custom Parameters
 #'
-#' This function saves the last ggplot object to a specified subdirectory
-#' within the current working directory. The function suppresses any messages
-#' generated during the save operation.
+#' This function serves as a wrapper for ggplot2's `ggsave` function, allowing for
+#' easy customization of the file path, resolution, and file type.
 #'
-#' @param filename The name of the file to save the plot as. Do not include the file extension.
-#' @param subdomain The subdirectory within the current working directory where the plot will be saved. Default is 'plots'.
+#' @param filename Character string specifying the name of the file.
+#' @param subdomain Character string specifying the subdirectory where the plot will be saved.
+#'                  Defaults to 'plots'.
+#' @param dpi Numeric value specifying the resolution of the saved plot in dots per inch.
+#'            Defaults to 300.
+#' @param type Character string specifying the file format. Accepts 'png', 'pdf', 'jpeg', etc.
+#'             Defaults to 'png'.
 #'
-#' @return NULL. The function is called for its side effect of saving a plot.
+#' @return NULL. The function saves the plot to disk and returns NULL.
+#'
 #' @examples
 #' \dontrun{
-#' # Create a simple ggplot object
-#' p <- ggplot(mtcars, aes(x = mpg, y = wt)) + geom_point()
-#' # Display the plot
-#' print(p)
-#' # Save the plot
-#' save_ggplot("my_plot")
+#'   # Create a simple ggplot2 plot
+#'   library(ggplot2)
+#'   p <- ggplot(mtcars, aes(x = mpg, y = wt)) + geom_point()
+#'   # Save the plot
+#'   save_ggplot('my_plot')
 #' }
+#'
+#' @importFrom ggplot2 ggsave
 #' @export
-save_ggplot <- function(filename, subdomain = 'plots') {
+save_ggplot <- function(filename, subdomain = 'plots', dpi = 300, type = 'png') {
   suppressMessages({
-    ggsave(file = file.path(
-      getwd(), subdomain, paste0(filename, '.png'))
-    )})
+    ggplot2::ggsave(
+      file = file.path(getwd(), subdomain, paste0(filename, '.', type)),
+      dpi = dpi,
+      device = type
+    )
+  })
 }
